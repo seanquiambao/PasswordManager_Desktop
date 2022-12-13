d4be997cb8dd23893a8df95a7cdc243ede2f77c7
Sean Quiambao <91030482+toastmeal@users.noreply.github.com>
2022-12-13 01:42:46 -0800

    Add project files.

---
+++ PasswordManager_Desktop/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PasswordManager_Desktop
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (isEmptyPrompt()) MessageBox.Show("Missing Fields", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(isEmptyPrompt()) MessageBox.Show("Missing Fields", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            string password = textBox2.Text;
+            string[] s = {textBox1.Text, textBox2.Text};
+
+
+
+
+        }
+
+        private bool isEmptyPrompt()
+        {
+            if (string.IsNullOrEmpty(textBox1.Text) || string.IsNullOrEmpty(textBox2.Text)) return true;
+            return false;
+        }
+ 
+    }
+}
---
+++ PasswordManager_Desktop/SQLDatabase.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows;
+
+namespace PasswordManager_Desktop
+{
+    class SQLDatabase
+    {
+        SqlConnection conn;
+
+        public SQLDatabase()
+        {
+            conn = new SqlConnection("Data Source = database.db; Version = 3; New = True; Compression = True;");
+            try
+            {
+                conn.Open();
+            }
+            catch
+            {
+                Console.WriteLine("Unable to connect to database");
+            }
+        }
+
+        void CreateUserDatabaseTable()
+        {
+            string s = "CREATE TABLE IF NOT EXIST 'User Database'(username STRING, password STRING, iterations INT)";
+            ExecuteNonQueryCommand(s);
+        }
+
+        void InsertTable(string[] columns, string tableName)
+        {
+            string s = $"INSERT INTO '{tableName}' VALUES(";
+            for(int i = 0; i < columns.Length; ++i)
+            {
+                s += $"'{columns[i]}', ";
+            }
+            s += ");";
+
+            ExecuteNonQueryCommand(s);
+        }
+
+        void ExecuteNonQueryCommand(string command)
+        {
+            SqlCommand cmd = new SqlCommand(command, conn);
+            cmd.ExecuteNonQuery();
+        }
+
+    }
+}
